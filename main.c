05c21f3d3cf1a350d76bbcda593c8eddc05a0126
josemgvr <jm.villora.2025@alumnos.urjc.es>
2026-04-08 18:20:32 +0200

    upload MAIN to test

--- main.c
+++ main.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include "tPolinomio.h"
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    tPolinomio *p1;
+    tPolinomio *p2;
+    tPolinomio suma;
+    tPolinomio derivada;
+    float x;
+
+    printf("Introduce el primer polinomio:\n");
+    p1 = leerPolinomio();
+
+    printf("\nIntroduce el segundo polinomio:\n");
+    p2 = leerPolinomio();
+
+    printf("\nPolinomio 1:\n");
+    mostrar(*p1);
+
+    printf("\nPolinomio 2:\n");
+    mostrar(*p2);
+
+    printf("\nDerivada del primer polinomio:\n");
+    derivada_polinomio(&derivada, *p1);
+    mostrar(derivada);
+
+    printf("\nIntroduce un valor para x: ");
+    scanf("%f", &x);
+    printf("Valor del polinomio 1 en x = %.2f: %.2f\n", x, valor(*p1, x));
+
+    printf("\nSuma de los polinomios:\n");
+    sumarPolinomios(&suma, *p1, *p2);
+    mostrar(suma);
+
+    // Liberar memoria
+    EliminartPolinomio(p1);
+    EliminartPolinomio(p2);
+    EliminartPolinomio(&suma);
+    EliminartPolinomio(&derivada);
+
     return 0;
 }
